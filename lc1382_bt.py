8aadf11e0838acbc85fbc9d7aa50c7c898037187
Mel <seuwxm@gmail.com>
2020-12-24 20:31:54 -0500

    Update lc1382_bt.py

--- lc1382_bt.py
+++ lc1382_bt.py
@@ -23,3 +23,25 @@ class Solution:
             inorder(root.right)
         inorder(root)
         return preorder(0, len(rslt))
+
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        rslt = []
+        def inorder(root):
+            if not root: return
+            inorder(root.left)
+            rslt.append(root)
+            inorder(root.right)
+            return
+
+        def preorder(l, r):
+            if l == r:
+                return None
+            mid = (l+r)//2
+            root = rslt[mid]
+            root.left = preorder(l, mid)
+            root.right = preorder(mid+1, r)
+            return root
+
+        inorder(root)
+        return preorder(0, len(rslt))
