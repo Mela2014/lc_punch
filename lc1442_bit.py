6215eabf1df954281875d2f150dd753208755ee1
Mel <seuwxm@gmail.com>
2021-02-04 20:24:42 -0500

    Update lc1442_bit.py

--- lc1442_bit.py
+++ lc1442_bit.py
@@ -1,14 +1,12 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
         rslt, n = 0, len(arr)
-        hold, temp = {0:(0, 0)}, 0
-        for i in range(n):
-            temp = temp ^ arr[i]
-            if temp in hold:
-                rslt += i-hold[temp][0]+hold[temp][1]*2 + (1 if hold[temp][1] else 0)
-                hold[temp] = (i+1, i-hold[temp][0])
-            else:
-                hold[temp] = (i+1, 0)
+        prefix = [0]*(n+1)
+        for i in range(n): prefix[i+1] = prefix[i] ^ arr[i]
+        for left in range(n-1):
+            for right in range(left+1, n):
+                if prefix[right+1]^prefix[left] == 0:
+                    rslt += right-left
         return rslt
 
 
