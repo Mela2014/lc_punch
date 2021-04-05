c9566b3957c7873bbcd035cadd623765d1d76f57
Mel <seuwxm@gmail.com>
2021-04-05 01:25:20 -0400

    241_bs_8

--- lc1283_bs.py
+++ lc1283_bs.py
@@ -11,3 +11,19 @@ class Solution:
             if mid == (left+right)//2:
                 left += 1
         return left
+
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def check(i):
+            rslt = 0
+            for n in nums:
+                rslt += (n-1)//i + 1
+            return rslt
+        left, right = 1, max(nums)
+        while left <= right:
+            mid = (left + right)//2
+            if check(mid) <= threshold:
+                right = mid -1
+            else:
+                left = mid + 1
+        return left
