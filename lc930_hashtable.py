3f778fb1c776639c5bcbbd07c4da18df36bb4497
Mel <seuwxm@gmail.com>
2021-05-25 00:03:00 -0400

    Update lc930_hashtable.py

--- lc930_hashtable.py
+++ lc930_hashtable.py
@@ -7,3 +7,23 @@ class Solution:
                 rslt += temp[prefix-S]
             temp[prefix] = temp.get(prefix, 0)+1
         return rslt
+    
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        left_lo, left_hi = 0, 0
+        last_lo, last_hi = 0, 0
+        rslt = 0
+        for right, num in enumerate(nums):
+            last_lo += num
+            last_hi += num
+
+            while left_lo < right and last_lo > goal:
+                last_lo -= nums[left_lo]
+                left_lo += 1
+            while left_hi < right and (last_hi > goal or (last_hi == goal and nums[left_hi] == 0)):
+                last_hi -= nums[left_hi]
+                left_hi += 1
+
+            if last_lo == goal:
+                rslt += left_hi - left_lo + 1
+        return rslt
